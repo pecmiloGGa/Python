ece48da9968f56eb0b5d9e34a3f65a55f13f68d4
pecmiloGGa <flipecmilo@gmail.com>
2025-11-23 19:52:55 -0600

    Update Television.py

--- Television.py
+++ Television.py
@@ -9,41 +9,46 @@ class Television:
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
+        self.__saved_volume = self.__volume  
 
     def power(self):
         self.__status = not self.__status
 
     def mute(self):
         if self.__status:
-            self.__muted = not self.__muted
+            if not self.__muted:
+                self.__muted = True
+                self.__saved_volume = self.__volume
+                self.__volume = 0
+            else:
+                self.__muted = False
+                self.__volume = self.__saved_volume
 
     def channel_up(self):
         if self.__status:
-            if self.__channel == Television.MAX_CHANNEL:
-                self.__channel = Television.MIN_CHANNEL
-            else:
-                self.__channel += 1
+            self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
 
     def channel_down(self):
         if self.__status:
-            if self.__channel == Television.MIN_CHANNEL:
-                self.__channel = Television.MAX_CHANNEL
-            else:
-                self.__channel -= 1
+            self.__channel = (self.__channel - 1) % (Television.MAX_CHANNEL + 1)
 
     def volume_up(self):
         if self.__status:
             if self.__muted:
                 self.__muted = False
+                self.__volume = self.__saved_volume
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
+            self.__saved_volume = self.__volume
 
     def volume_down(self):
         if self.__status:
             if self.__muted:
                 self.__muted = False
+                self.__volume = self.__saved_volume
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
+            self.__saved_volume = self.__volume
 
     def __str__(self):
         return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
